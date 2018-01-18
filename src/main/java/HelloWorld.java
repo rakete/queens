b952b91b3241c44b5640c3932bffb8713eb56e6e
Andreas Raster <lazor@affenbande.org>
2018-01-18 19:24:39 +0100

    fix file header license generation

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 /**
- * Copyright ${year} Shevek
+ * Copyright 2018 Andreas Raster
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,11 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
  */
 package org.rastermann.compilerworks;
 
