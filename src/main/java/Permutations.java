80098d90ebaf46cb46fa13da418aea64147b675d
Andreas Raster <lazor@affenbande.org>
2018-01-19 20:39:20 +0100

    add comments to the fiddly bits

--- src/main/java/Permutations.java
+++ src/main/java/Permutations.java
@@ -50,6 +50,8 @@ public class Permutations {
             return 0;
         }
 
+        // - anything larger than 20 would produce an integer overflow, so I just throw an exception
+        // in that case, same as with negative arguments
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException();
         }
@@ -66,6 +68,31 @@ public class Permutations {
             return f.apply(permutation);
         }
 
+        // - found this method here:
+        // https://stackoverflow.com/questions/30387185/print-out-all-permutations-of-an-array
+        // - I changed it a little, adding the if condition and an assert to make it more clear
+        // how this works
+        // - I also added a boolean return value to this function and the f argument so that the
+        // caller can abort permuting by returning false
+        // - this is a mix between an iterative an recursive method, which makes it relativly easy
+        // to understand, but still fast
+        // - on the first invocation with i = 0, the if above will do nothing, the for loop is started
+        // with j = i = 0, which then means j == i and the else condition below is followed. this continues
+        // until we reach the end of the permutation array, then the above if triggers and applies
+        // f to the current permutation array, which is at this point completly unchanged, therefore
+        // the first time f is called with just [0, 1, 2, ...., n] as input
+        // - we then return to the previous call, where the for loop runs once, increases j, so that now
+        // j > i and the main if body is triggered, where we swap two elements of the array, then call
+        // ourself recursively, again triggering the above if and applying f to permutation with the last
+        // two elements swapped
+        // - now again we return to the for loop that already ran increasing j to j > i, now it would
+        // increase once more, but that would make j >= permutation.length, which means instead we
+        // return true below, go the the previous call again (the second to last one), increase j,
+        // then swap two elements (the third to last with the second to last), and call ourself
+        // recursivly again
+        // - this continues until in the very first call, j ran from i = 0 to permutation.length, after
+        // which the first element was switched with every other element at least once, which means
+        // there are no more possible enumerations left, and we are done
         for (int j = i; j < permutation.length; j++) {
             assert j >= i;
             if (j > i) {
--- src/main/java/Queens.java
+++ src/main/java/Queens.java
@@ -50,11 +50,45 @@ public class Queens {
     }
 
     public boolean test(Integer[] config) {
+        // - this is where the magic happens
+        // - the queens are initialized as array of integers, where each integer has exactly one bit
+        // set for a queen in a row, and all integers in the array are different, so something like
+        // this for n = 4:
+        // 0: 1 0 0 0
+        // 1: 0 1 0 0
+        // 2: 0 0 1 0
+        // 3: 0 0 0 1
+        // - a config is a board configuration determined by an ordering of rows, so something like
+        // [2, 0, 3, 1] would represent a board like this:
+        // 2: 0 0 1 0
+        // 0: 1 0 0 0
+        // 3: 0 0 0 1
+        // 1: 0 1 0 0
+        // - to determine if a config is a valid solution we have to test if there are any two rows
+        // with queens that threaten each other, we do not have to test if there are two queens in
+        // a single row or column, because that is impossible, we only have to test if there are
+        // two queens on a diagonal
+        // - two rows threaten each other on the diagonal when one of them bitshifted left or right
+        // by the amount of rows they are apart, equals the value of the other
+        // - so with config [2, 0, 3, 1], to test if the i = 0 row (a), threatens the j = 1 row (b), we
+        // get their indices from the config (config[i] = 2 and config[j] = 0), then get the row values
+        // for a = queens[config[i]] = [0 0 1 0] and b = queens[config[j]] = [1 0 0 0], then compute
+        // how much the rows are apart d = j - i = 1, and then we bitshift the row value a left and
+        // right by d and test if either of the two resulting values are equal to b:
+        // [0 0 1 0] >> 1 = [0 0 0 1] != [1 0 0 0] and [0 0 1 0] << 1 = [0 1 0 0] != [1 0 0 0]
+        // - in this case they are not equal, which means we can continue testing rows against each other
+        // in this fashion, if they were equal it would mean the queens in these rows threaten each
+        // other, and we can discard the whole config because it cannot possibly be a valid solution
+        // anymore
+        // - notice that j only goes forward, we do not need to test row combinations twice, so if
+        // we tested the 0 vs 1, we do not need to test 1 vs 0 as well, by only increasing j from
+        // i to config.length, and not decreasing it as well, we only test the combinations the we
+        // have not tested yet
         for (int i = 0; i < config.length - 1; i++) {
-            for (int k = i + 1; k < config.length; k++) {
+            for (int j = i + 1; j < config.length; j++) {
                 int a = queens[config[i]];
-                int b = queens[config[k]];
-                int d = k - i;
+                int b = queens[config[j]];
+                int d = j - i;
 
                 if (b == (a >> d) || b == (a << d)) {
                     return false;
